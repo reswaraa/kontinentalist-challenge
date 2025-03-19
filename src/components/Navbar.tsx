50694f29d3e345ad5acfe7a8831fe160b3ba9c26
reswaraa <reswara.ad@gmail.com>
2025-03-20 07:51:19 +0800

    feat(navbar): add pathname

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,40 @@
 'use client';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export default function Navbar() {
+  const pathname = usePathname();
+  const isActive = (path: string) => {
+    return pathname === path;
+  };
   return (
     <nav className="text-white shadow-md">
       <div className="container mx-auto px-4">
         <div className="flex items-center justify-between h-16">
           <div className="font-bold text-xl">Kontinentalist Challenge</div>
           <div className="flex space-x-4">
-            <Link href="/" className="px-3 py-2 rounded-md font-medium">
+            <Link
+              href="/"
+              className={`px-3 py-2 border border-gray-700 rounded-md font-medium ${
+                isActive('/') ? 'bg-gray-700' : 'hover:bg-gray-700'
+              }`}
+            >
               Home
             </Link>
-            <Link href="/stories" className="px-3 py-2 rounded-md font-medium">
+            <Link
+              href="/stories"
+              className={`px-3 py-2 border border-gray-700 rounded-md font-medium ${
+                isActive('/stories') ? 'bg-gray-700' : 'hover:bg-gray-700'
+              }`}
+            >
               Stories
             </Link>
-            <Link href="/posts" className="px-3 py-2 rounded-md font-medium">
+            <Link
+              href="/posts"
+              className={`px-3 py-2 border border-gray-700 rounded-md font-medium ${
+                isActive('/posts') ? 'bg-gray-700' : 'hover:bg-gray-700'
+              }`}
+            >
               Posts
             </Link>
           </div>
